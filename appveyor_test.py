a77f5a6e228ad86b46472d7426b6bc5e7fb59a62
Zachary Ware <zachary.ware@gmail.com>
2017-05-29 13:40:10 -0500

    Update appveyor_test.py

--- appveyor_test.py
+++ appveyor_test.py
@@ -7,6 +7,7 @@ class Test(unittest.TestCase):
     def test_UNC_path(self):
         # Create the UNC path, like \\myhost\c$\foo\bar.
         path = os.path.dirname(os.path.abspath(__file__))
+        self.assertIn(os.path.basename(__file__), os.listdir(path))
         hn = socket.gethostname()
         drive = path[0]
         unc = "\\\\%s\\%s$"%(hn, drive)
